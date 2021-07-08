3fd1ae00838a95dc14b97435404452b8d66b155f
Rivka Tzur <rivkat@linnovate.net>
2021-07-08 08:33:17 +0300

    example addUpdateEntitiesTypes

--- src/app/example.js
+++ src/app/example.js
@@ -52,7 +52,7 @@ module.exports = `
 #  }
  
 #  mutation {
-#    addUpdateDeviceTypes(
+#    addUpdateEntitiesTypes(
 #        uid: "1f596b40-77c4-11e9-84ec-4b23f2be1633",
 #        id: "device1",
 #        name: "device1",
